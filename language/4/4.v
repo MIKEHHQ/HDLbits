55ad826eab88b5f20d136b00e3500281dddba5f1
mikehhq <mikehhq@qq.com>
2022-03-15 17:06:13 +0800

    update

--- language/4/4.v
+++ language/4/4.v
@@ -1,7 +1,11 @@
-module top_module( 
-    input a,b,c,
-    output w,x,y,z );
-	assign w = a;
+module top_module(input a,
+                  input b,
+                  input c,
+                  output w,
+                  output x,
+                  output y,
+                  output z);
+    assign w = a;
     assign x = b;
     assign y = b;
     assign z = c;
